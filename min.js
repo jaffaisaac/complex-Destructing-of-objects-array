5090033903c1bc5ee7fe427727ceef36e60edf55
jaffa Isaac <jaffaisaac4@gmail.com>
2023-02-23 15:12:33 +0300

    Add files via upload
    This code extracts student.courses.english and student.scoreRange[0] into 
    english and minScore, respectively. In this example both courses and scoreRange
    are variables in the destructured pattern that get their values from the corresponding 
    properties within the student object.

---
+++ min.js
@@ -0,0 +1,35 @@
+let Student ={
+    name: 'Jaffa',
+    Sujects:{
+        math:{
+            id:001,
+            score: 30
+        },
+        SST:{
+            id:002,
+            score:60
+        },
+        English:{
+            id:003,
+            score:90
+        },
+        Science:{
+            id:004,
+            score:80
+
+        }
+
+    },
+    scoreRange:[0,100]
+}
+
+let{
+ Sujects:{Science ,English},
+ scoreRange:[,height ]
+
+
+} =Student
+console.log({Science},[height]);
+console.log(English.id)
+
+console.log(English.score = 15)
